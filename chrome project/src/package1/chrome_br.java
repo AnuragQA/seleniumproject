24390c49fe2ce5e0b60f3277c0466275a98a9a58
AnuragQA <anuragh.qa@gmail.com>
2017-12-12 15:53:50 +0530

    Modified the code

--- chrome project/src/package1/chrome_br.java
+++ chrome project/src/package1/chrome_br.java
@@ -6,12 +6,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class chrome_br {
 
 	public static void main(String[] args) throws InterruptedException {
-	
+	    
+		//Setting up property for chrome browser
 		System.setProperty("webdriver.chrome.driver","C:\\Program Files\\chromedriver.exe");
+		
+		//launching chrome browser
 		WebDriver d=new ChromeDriver();
+		
+		//launching the application homepage
 		d.get("https://www.google.co.in/");
+		
+		//maximizing the window 
 		d.manage().window().maximize();
-        Thread.sleep(2000);
+        
+		//applying the wait statement
+		Thread.sleep(2000);
+        
+		//closing the current window
         d.close();
 		
 	}
